c6e685aca60f7e3aa1372e0c8703ea5501c6cd27
Luana Melo <eliezermiguel97@outlook.com>
2022-07-02 22:11:10 -0300

    Validação prontagit add .

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ function verificar(){
     var altura = (Number(txtalt.value))/100
     var peso  = Number(txtpes.value)
     var imc = peso / altura ** 2
-    if (imc < 18.5){
-        res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está abaixo do peso!`
-    } else if (imc < 24.9){
-        res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está no peso ideal`
-    } else if (imc < 29.9){
-        res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está acima do peso!`
-    } else if (imc < 34.9){
-        res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está com obesidade do grau I!`
-    } else if (imc <39.9){
-        res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está com obesidade do grau II`
+    if (txtalt.value == 0 || txtpes.value == 0){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está com obesidade morbida`
+        if (imc < 18.5){
+            res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está abaixo do peso!`
+        } else if (imc < 24.9){
+            res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está no peso ideal`
+        } else if (imc < 29.9){
+            res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está acima do peso!`
+        } else if (imc < 34.9){
+            res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está com obesidade do grau I!`
+        } else if (imc <39.9){
+            res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está com obesidade do grau II`
+        } else {
+            res.innerHTML = `Seu IMC é ${imc.toFixed(2)} e você está com obesidade morbida`
+        }
     }
-}
+    }
